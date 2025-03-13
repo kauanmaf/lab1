890e80b18d04a0a33d07a692bbf23e05328c68f9
Kauan <kauan.mariani@gmail.com>
2025-03-13 12:41:31 -0300

    adicionando final

--- global.js
+++ global.js
@@ -1,26 +1,29 @@
-console.log("IT'S ALIVE");
+// console.log("IT'S ALIVE");
 
-var x=0;
+// var x=0;
 
-x = x+3
+// x = x+3
 
-console.log(x)
+// console.log(x)
 
 
-function $$ (selector, context = document) {
-    return Array.from(context.querySelectorAll(selector));
-}
+// function $$ (selector, context = document) {
+//     return Array.from(context.querySelectorAll(selector));
+// }
 
-let navLinks = $$("nav a");
-let currentLink = navLinks.find(a => a.host === location.host && a.pathname === location.pathname);
-currentLink?.classList.add("current");
-console.log(navLinks);
+// let navLinks = $$("nav a");
+// let currentLink = navLinks.find(a => a.host === location.host && a.pathname === location.pathname);
+// currentLink?.classList.add("current");
+// console.log(navLinks);
+
+const ARE_WE_HOME = document.documentElement.classList.contains("home");
 
 let pages = [
-    {url: "../", title : "Home"},
-    {url: "../contact/", title : "contact"},
-    {url: "../projects/", title : "Projects"}
-    
+    {url: "", title : "Home"},
+    {url: "projects/", title : "Projects"},
+    {url: "contact/", title : "Contact"},
+    {url: "https://github.com/kauanmaf", title : "GitHub"}
+
 ];
 
 let nav = document.createElement("nav");
@@ -29,6 +32,21 @@ document.body.prepend(nav);
 for (let p of pages) {
     let url = p.url;
     let title = p.title;
+    if (!ARE_WE_HOME && !url.startsWith("http")) {
+        url = "../" + url;
+    }
     // Create link and add it to nav
-    nav.insertAdjacentHTML("beforeend", `<a href="${ url }">${ title }</a>` );
+    let a = document.createElement("a");
+    a.href = url;
+    a.textContent = title;
+    if (a.host === location.host && a.pathname === location.pathname) {
+        a.classList.add("current");
+    }
+    
+    if (a.hsot != location.host){
+        a.setAttribute("target", "_blank")
+    }
+
+    nav.append(a);
 }
+
